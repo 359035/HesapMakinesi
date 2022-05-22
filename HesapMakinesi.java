2f1ca06c7f9fa4b6b9ead53ac20ea454095e2ece
Ibrahim M <105627112+359035@users.noreply.github.com>
2022-05-22 22:48:02 +0300

    Add files via upload

---
+++ HesapMakinesi.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hesapmakinesi;
+
+/**
+ *
+ * @author Samsung
+ */
+
+import java .util.Scanner;
+public class HesapMakinesi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a,b,secim;
+        Scanner inp=new Scanner(System.in);
+        
+        System.out.print("İlk Sayıyı Giriniz:");
+        a=inp.nextInt();
+        
+        System.out.print("İkinci Sayıyı Giriniz:");
+        b=inp.nextInt();
+        
+        System.out.println("1.Toplama\n2.Çıkarma\n3.Çarpma\n4.Bölme");
+        System.out.print("Lütfen Seçiminizi Giriniz:");
+        secim=inp.nextInt();
+        
+        switch(secim){
+            case 1 -> System.out.println("Toplam:"+(a+b));
+            case 2 -> System.out.println("Çıkarma:"+(a-b));
+            case 3 -> System.out.println("Çarpma:"+(a*b));
+            case 4 -> {
+                if(b=0){
+                    System.out.println("Bir sayı sıfıra bölünemez!");
+                    
+                }
+                else{
+                    System.out.println("Bölme:"+(a/b));
+                }
+            }
+            default -> System.out.println("Hatalı Seçim");
+                
+                
+                
+        }
+        
+    }
+    
+}
